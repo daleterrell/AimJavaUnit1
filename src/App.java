bc2fcafd0bc33733ae6de35d1e9e8749a6c280b0
Dale Terrell <tragicid@gmail.com>
2022-05-09 18:26:27 -0500

    homeverk

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!"); 
+    public static void main(String[] args) throws Exception { 
         System.out.println("    J     A   V       V   A");
         System.out.println("    J    A A   V     V   A  A");
         System.out.println("    J   A   A   V   V   A    A");
