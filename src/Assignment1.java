842c7f832f1e2e90d6f438fcc9cf4c785983f6d2
Dale Terrell <tragicid@gmail.com>
2022-05-18 15:14:43 -0500

    moved some things

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception { 
+    public static void main(String[] args) { 
         String logo = "     J    A    V     V     A \n" +
                       "     J   A A    V   V     A A \n" +
                       "J    J  AAAAA    V V     AAAAA \n" +
