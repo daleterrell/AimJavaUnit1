981a8876c6c8ec4131cda3891d7740debcb52e1f
Dale Terrell <tragicid@gmail.com>
2022-05-18 10:40:20 -0500

    moved some things

---
+++ src/Assignment1.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception { 
+        System.out.println("    J     A   V       V   A");
+        System.out.println("    J    A A   V     V   A  A");
+        System.out.println("    J   A   A   V   V   A    A");
+        System.out.println("J   J  AAAAAAA   V V   AAAAAAAA");
+        System.out.println(" j  J A       A   V   A        A");
+        
+    }
+}
---
+++ src/Assignment1_2.java
@@ -0,0 +1,9 @@
+public class Assignment1_3 {
+    public static void main(String[] args) { 
+        System.out.print("    Kyle Schulz");
+        System.out.print("    Java Web");
+        System.out.print("    Omaha, NE");
+        System.out.print("    Apple Crisp Pie");
+    }   
+}
+
